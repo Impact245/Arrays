9968b559cde2ecf9754e9424be6b5025b9069371
Impact245 <Beasley821@gmail.com>
2017-10-19 12:48:08 -0400

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] j = new String[100];
+
+            for (int i = 0; i < j.Length; i++)
+            {
+                j[i] = i + ".";
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    j[i] = i + ". FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    j[i] = i + ". Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    j[i] = i + ". Buzz";
+                }
+                Console.WriteLine(j[i]);
+            }
+        }
+    }
+}
