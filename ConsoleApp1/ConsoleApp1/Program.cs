4515d309e9ff2cd6e8af79745cb3af94e67ab3af
Impact245 <Beasley821@gmail.com>
2017-10-25 11:05:16 -0400

    Fast Eddie (Dedupe)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 0;
+
+            int[] j = new int[50];
+            Random k = new Random();
+
+            for (int i = 0; i < j.Length; i++)
+            {
+                j[i] = k.Next(1, 50);
+            }
+
+            for (int l = 0; l < j.Length - 1; l++)
+            {
+                for (int h = l + 1; h < j.Length; h++)
+                {
+                    if (j[l] == j[h])
+                    {
+                        j[h] = -1;
+                    }
+                }
+            }
+
+            for (int y = 0; y < j.Length; y++)
+            {
+                if (j[y] != -1)
+                {
+                    t++;
+                }
+            }
+
+            Console.WriteLine("Length is now " + t);
+            
+
+
+
+        }
+    }
+}
